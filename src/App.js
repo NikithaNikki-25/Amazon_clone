4b0e4bf0c17a1532122c2e4b4700fcd7cc0118de
Nikitha Nikki <Nikitha.nikki525@gmail.com>
2023-07-13 00:21:48 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/*import logo from './logo.svg';*/
+import './App.css';
+import MainPage from './components/HomePage/MainPage';
+import Navbar from './components/NavBar/navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <MainPage/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/navigation.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import "./NavBar.css";
+
+class Navbar extends Component {
+    constructor(props){
+        super(props);
+        this.state={ }
+    }
+    render(){
+        return(
+            <div className="navbar_component">
+                Navbar
+            </div>
+        );
+    }
+}
+export default Navbar;
