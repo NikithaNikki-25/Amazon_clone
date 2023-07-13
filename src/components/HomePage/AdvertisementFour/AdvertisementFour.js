a88777a78bf4de002e97683325fe74583111ed0f
Nikitha Nikki <Nikitha.nikki525@gmail.com>
2023-07-14 00:33:16 +0530

    added

---
+++ src/components/HomePage/AdvertisementFour/AdvertisementFour.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./AdvertisementFour.css";
+
+function AdvertisementFour(props){
+    return(
+        <div className="AdvertisementOne_main">
+            <div className="AdvertisementOne_header">
+            Up to 70% off | Electronics Clearance store
+            </div>
+            <div className="AdvertisementOne_body">
+                <img src="https://ik.imagekit.io/8nc25svc9/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1689226327586" className="ad_fourImage"/>
+                <img src="https://ik.imagekit.io/8nc25svc9/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1689226327586" className="ad_fourImage"/>
+                <img src="https://ik.imagekit.io/8nc25svc9/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1689226327586" className="ad_fourImage"/>
+                <img src="https://ik.imagekit.io/8nc25svc9/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1689226327586" className="ad_fourImage"/>
+            </div>
+            <div className="AdvertisementOne_footer">
+                See more
+            </div>
+
+
+        </div>
+    )
+}
+export default AdvertisementFour;
---
+++ src/components/HomePage/AdvertisementOne/AdvertisementOne.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./AdvertisementOne.css";
+function AdvertisementOne (props){
+    return (
+        <div className="AdvertisementOne_main">
+            <div className="AdvertisementOne_header">
+            Up to 70% off | Electronics Clearance store
+            </div>
+            <div className="AdvertisementOne_body">
+                <img src="https://ik.imagekit.io/8nc25svc9/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1689226327586" width="300px"/>
+            </div>
+            <div className="AdvertisementOne_footer">
+                See more
+            </div>
+
+
+        </div>
+    )
+}
+export default AdvertisementOne;
